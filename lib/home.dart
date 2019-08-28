e1d1dcaac08b5de01a47c3a0a0ac412b64e69c57
bruno pf silva <bruno.pfsilva@hotmail.com>
2019-08-29 00:59:43 +0100

    app concluido

--- lib/home.dart
+++ lib/home.dart
@@ -5,18 +5,9 @@ import 'package:flutter_app_preco_bitcoin_bloc/home_bloc.dart';
 class homePage extends StatefulWidget {
   @override
   _homePageState createState() => _homePageState();
-
-
-
-
 }
 
-
-
-
 class _homePageState extends State<homePage> {
-
-
   homeBloc bloc;
 
   @override
@@ -36,14 +27,37 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StreamBuilder<Object>(
+      body: StreamBuilder<Object>(
           stream: bloc.out,
+          initialData: 0,
           builder: (context, snapshot) {
             return Container(
-
+              padding: EdgeInsets.all(32.0),
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset("images/bitcoin.png"),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30, bottom: 30),
+                      child: Text(
+                        "R\$ " + snapshot.data,
+                        style: TextStyle(fontSize: 36),
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text(
+                        "Atualizar",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                      color: Colors.orange,
+                      padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                      onPressed: this.bloc.recuperarpreco,
+                    ),
+                  ],
+                ),
+              ),
             );
-          }
-        ),
-      );
+          }),
+    );
   }
 }
--- lib/home_bloc.dart
+++ lib/home_bloc.dart
@@ -1,20 +1,35 @@
 import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class homeBloc {
 
-  int bitcoinPrice = 0;
+  double bitcoinPrice = 0;
 
-  var _priceStreamController = new StreamController<int>();
+  var _priceStreamController = new StreamController<double>();
 
-
+  String url = "https://blockchain.info/ticker";
 
   //saida do stream
-  Stream<int> get out => _priceStreamController.stream;
+  Stream<double> get out => _priceStreamController.stream;
   //entrada do stream
-  Sink<int> get inn => _priceStreamController.sink;
+  Sink<double> get inn => _priceStreamController.sink;
 
 
 
+  void recuperarpreco () async {
+
+    http.Response response = await http.get(url);
+
+    Map<String,dynamic> retorno = json.decode( response.body );
+
+    bitcoinPrice = retorno["BLR"]["buy"];
+
+    //Entra o preco no stream
+    //_priceStreamController.sink.add(bitcoinPrice);
+    _priceStreamController.sink.add(bitcoinPrice);
+  }
 
 
   void dispose(){
